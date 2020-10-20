907705d3ac3dfc0cf9ead0cacb6da9ff9c8b3195
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-20 19:15:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,14 +8,15 @@ def turn_count(board)
   puts "#{counter}"
   end
 end
+end
 
 def current_player(board)
   counter = 0
   turn_count(board)
-  if counter.even? == true
-    current_player = "O"
-  else    
+  if counter.odd? == true
     current_player = "X"
+  else    
+    current_player = "O"
   end
 end
 puts "#{current_player}"
