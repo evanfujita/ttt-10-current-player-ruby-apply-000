b5d0ec27a8e75af30c710a72882f97b6dde59794
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-20 19:20:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 board = ["X", " ", "O", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-  counter = 0
+  counter = 1
   board.each do | moves |
     if moves == "X" || moves == "O"
   counter += 1
@@ -11,7 +11,7 @@ end
 end
 
 def current_player(board)
-  counter = 0
+  counter = 1
   turn_count(board)
   if counter.odd? == true
     current_player = "X"
