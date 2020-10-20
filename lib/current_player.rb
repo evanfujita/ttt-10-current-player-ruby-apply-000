8c5823404e0ceb4c693cb6b39369983a3f30a1ba
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-20 19:14:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 board = ["X", " ", "O", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
+  counter = 0
   board.each do | moves |
     if moves == "X" || moves == "O"
   counter += 1
@@ -18,22 +19,3 @@ def current_player(board)
 end
 puts "#{current_player}"
 end
-
-
-
-
-
-def current_player(board)
-  counter = 0
-  while counter < 9
-  turn_count(board)
-  if counter.even? == true
-    current_player = "O"
-  else    
-    current_player = "X"
-  end
-end
-puts "#{current_player}"
-end
-
-
